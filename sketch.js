e4ebe4719121d04876a372b93f2322593bf5332c
whosaidthat-3636 <jong788@aucklanduni.ac.nz>
2025-01-28 10:10:38 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -9,19 +9,19 @@ function setup() {
   background(220);
 }
 
-function allMyHours(){
-  noStroke(0);
-  fill(249, 56, 39);
-  let xPos = 30;
-  for(x = 0; x < table.getRowCount(); x++){
-    let row = table.getRow(x);
-    let countWeek = row.get("Week");
-    if (countWeek > 0){
-      for (y = 0; y < countWeek; y++){
-        ellipse(xPos, 50, 20, 20);
-        xPos += 20;
-      }
-    }
+// function allMyHours(){
+//   noStroke(0);
+//   fill(249, 56, 39);
+//   let xPos = 30;
+//   for(x = 0; x < table.getRowCount(); x++){
+//     let row = table.getRow(x);
+//     let countWeek = row.get("Week");
+//     if (countWeek > 0){
+//       for (y = 0; y < countWeek; y++){
+//         ellipse(xPos, 50, 20, 20);
+//         xPos += 20;
+//       }
+//     }
 //     let countHours = row.get("Hours");
 //     if (countHours> 0){
 //       for ( y=0; y < countHours; y++){
@@ -29,18 +29,18 @@ function allMyHours(){
 //         xPose += 20;
 //       }
 //     }
-  }
-  }
+  // }
+  // }
 
-// function weekLabel() {
-//   for(x = 0; x < table.getRowCount(); x++){
-//     let row = table.getRow(x);
-//     let week = row.get("Week");
-//     fill(0);
-//     text(week, 145 + x * 80, 350);
-//     image
-//   }
-// }
+function weekLabel() {
+  for(x = 0; x < table.getRowCount(); x++){
+    let row = table.getRow(x);
+    let week = row.get("Week");
+    fill(0);
+    text(week, 145 + x * 80, 350);
+    image
+  }
+}
 
 // function hoursLabel() {
 //   for(y = 0; y < table.getRowCount(); y++){
@@ -51,35 +51,34 @@ function allMyHours(){
 //   }
 // }
 
-// function showChart(){
-//   //array
-//   let colors = [
-//     color(249, 56, 39), // week 1: red
-//     color(255, 257, 35), // week 2: orange
-//     color(255, 214, 90), // week 3: yellow
-//     color(22, 196, 127), // week 4: green
-//   ];
+function showChart(){
+  //array
+  let colors = [
+    color(249, 56, 39), // week 1: red
+    color(255, 257, 35), // week 2: orange
+    color(255, 214, 90), // week 3: yellow
+    color(22, 196, 127), // week 4: green
+  ];
 
-//   for(x = 0; x < table.getRowCount(); x++){
-//     let row = table.getRow(x);
-//     let barChart = row.get("Hours");
-//     fill(colors[x]);
-//     noStroke();
-//     rect(135 + x * 80, 320, 30, -barChart * 10); 
-//   }
-// }
+  for(x = 0; x < table.getRowCount(); x++){
+    let row = table.getRow(x);
+    let barChart = row.get("Hours");
+    fill(colors[x]);
+    noStroke();
+    rect(135 + x * 80, 320, 30, -barChart * 10); 
+  }
+}
 
 function draw() {
   fill(0);
-  // text('Week', 260, 380);
-  // text('Hours Played', 25, height/2);
-  // text('Hours of Music Played in a Month', 160, 420);
-  // text('30', 110, 31);
-  // text('15', 110, 181)
-   textSize(14);
-   allMyHours();
-  // weekLabel();
-  // showChart();
+  text('Week', 260, 380);
+  text('Hours Played', 25, height/2);
+  text('Hours of Music Played in a Month', 160, 420);
+  text('30', 110, 31);
+  text('15', 110, 181)
+  //  allMyHours();
+  weekLabel();
+  showChart();
 
 
 }
